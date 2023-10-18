68a005462897e88d9cb0f92fc922822d85d94ba9
Octorok192 <awmcco893@i.softbank.jp>
2023-10-18 20:22:22 +0900

    プレイヤーの仮の動きを作成

---
+++ Assets/Script/PlayerMove.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] float _movePower;
+    [SerializeField] Vector3 _moveInput;
+    Rigidbody _rb = default;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        _rb.velocity = new Vector3(_moveInput.x * _movePower, _rb.velocity.y , _moveInput.y * _movePower);
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        _moveInput = context.ReadValue<Vector3>();
+    }
+}
